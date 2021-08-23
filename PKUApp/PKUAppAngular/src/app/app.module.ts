6e0d4630631a5dd00bd508e054b67f8354948d45
Maciek <mjstyly@gmail.com>
2021-08-23 20:57:08 +0200

    navbar

--- PKUApp/PKUAppAngular/src/app/app.module.ts
+++ PKUApp/PKUAppAngular/src/app/app.module.ts
@@ -43,7 +43,7 @@ export function tokenGetter() {
     ShowProductsComponent,
     AddEditProductComponent,
     DetailsProductComponent,
-    DeleteProductComponent
+    DeleteProductComponent,
   ],
   imports: [
     BrowserModule,
